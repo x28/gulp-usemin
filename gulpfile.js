d75224c2d1f490cc6a3a9825a391da4a77ce0ce8
Lukas Loehrer <lukas.loehrer@x28.ch>
2020-11-03 18:04:41 +0100

    [npm] Update to gulp 4 and get rid of gulp-util dependency

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-
-gulp.task('lint', function() {
+function lint() {
 	return gulp.src('test/main.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'))
 		.pipe(mocha());
-});
+}
 
-gulp.task('default', ['lint']);
+exports.default = lint;
